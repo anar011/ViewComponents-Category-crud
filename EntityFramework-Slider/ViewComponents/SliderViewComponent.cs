3765c9ac8742f855f5db323c0e72a96f5f4409a8
anaraa@code.edu.az <you@example.com>
2023-04-18 15:41:50 -0700

    ViewComponents

---
+++ EntityFramework-Slider/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,21 @@
+﻿//using EntityFramework_Slider.Data;
+//using EntityFramework_Slider.Models;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace EntityFramework_Slider.ViewComponents
+//{
+//    public class SliderViewComponent : ViewComponent
+//    {
+//        private readonly AppDbContext _context;
+//        public SliderViewComponent(AppDbContext context)
+//        {
+//            _context = context;
+//        }
+//        public async Task<IViewComponentResult> InvokeAsync()
+//        {
+//            List<Slider> sliders = await _context.Sliders.ToListAsync();
+//            return await Task.FromResult(View(sliders));
+//        }
+//    }
+//}
