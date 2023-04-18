8bd07e1e5faef192de16e5f54afbb4e70f26d88a
anaraa@code.edu.az <you@example.com>
2023-04-18 08:34:47 -0700

    Crud

---
+++ EntityFramework-Slider/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,10 @@
+﻿using EntityFramework_Slider.Models;
+
+namespace EntityFramework_Slider.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAll(); 
+
+    }
+}
